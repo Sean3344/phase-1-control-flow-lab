bf92aa66e5e88c9414dfe438d921e56112ba60d6
Sean Nekouei <sean.nekouei@gmail.com>
2023-01-08 19:39:24 -0700

    commit tree a292b7fdda6a0f2509e4cc72cff835ccac470191

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(value) {
+  if (value <= 400 ) {
+  return 'This one is on me!'
+}
+  else if (value > 400 && value <= 2000) {
+    return 'That will be twenty bucks.';
+  }
+  else if (value > 2000 && value <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else (value > 2500)  
+    return 'No can do.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === 'NYC') {
+    return 'Ok, sounds good.'
+  }
+  else (city !== 'NYC') 
+    return 'No go.' 
+}
+const tip = 'generous'
+function switchOnCharmFromTip(tip) {
+switch (tip) {
+  case 'generous':
+  return 'Thank you so much.';
+  case 'not as generous': 
+  return 'Thank you.';
+  default:
+    return 'Bye.';
+}
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
