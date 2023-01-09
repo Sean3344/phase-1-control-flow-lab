4c0e3c2e7ddc42e240e2fcc2e32a86b943b0f4e4
Sean Nekouei <sean.nekouei@gmail.com>
2023-01-09 10:41:25 -0700

    first re-code commit

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(sample){
+  if(sample <= 400)
+  return "This one is on me!"
+  else if(sample >400 && sample <=2000){
+    return "That will be twenty bucks."
+  }
+  else if(sample > 2000 && sample <= 2500) {
+    return "I will gladly take your thirty bucks." 
+  }
+  else {
+    return "No can do."
+  }
+  }
+  // Write your code here!
+
+
+function ternaryCheckCity(city){
+  if(city === "NYC") {
+    return "Ok, sounds good."
+  }
+  else {
+    return "No go."
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case "generous" :
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default : 
+    return "Bye."
+  }
 
-function switchOnCharmFromTip(){
   // Write your code here!
 }
